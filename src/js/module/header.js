3a3d2eda857c97552bb5bde70ce581d9f54bd50c
DopamineQ <1848986723@qq.com>
2019-07-08 10:54:35 +0800

    轮播图完成

--- src/js/module/header.js
+++ src/js/module/header.js
@@ -2,12 +2,33 @@ define(['jquery'], function() {
     class Header {
         constructor () {
             this.container = $('header')
-            this.init()
+            this.init().then( () => {
+                //因为购物车的DOM是加载进去的（异步），所以我们要在then里面获取
+                this.cartWrap = $('.cart-num')
+                //console.log(this.cartWrap)
+                this.calcTotalNum()
+            })
         }
         init() {
             return new Promise(resolve => {
                 this.container.load('/html/module/header.html',resolve)
             })
+        }
+
+        //计算购物车的总数量
+        calcTotalNum () {
+            let cart = localStorage.getItem('cart')
+            if (cart){
+                cart = JSON.parse(cart)
+                //总种类 || 总数量
+                let totalNum = cart.reduce( (num, shop) => {
+                    num += shop.num
+                    return num
+                },0)
+                this.cartWrap.html(totalNum)
+            }else{
+                this.cartWrap.html(0)
+            }
         }
     } 
     return new Header()
--- src/js/shopcart.js
+++ src/js/shopcart.js
@@ -1,12 +1,14 @@
 require(['./config'], function () {
-    require(['template', 'header', 'footer','aside'], (template) => {
+    require(['template', 'header', 'footer','aside'], (template, header) => {
         class Cart{
             constructor () {
                 this.container = $('#cart-box')
                 this.totalPrice = $('.totalPrice')
+                this.allCheck = $('.all-check-box')
                 this.init()
-                this.calcMoney ()
-                this.checkChange ()
+                this.calcMoney()
+                this.checkChange()
+                this.allCheckChange()
             }
 
             init () {
@@ -19,6 +21,13 @@ require(['./config'], function () {
                 }else{
                     this.container.html("<a href='/' style='display:block; color:green; font-size:25px; text-align:center'>快去买东西吧！</a>")
                 }
+
+                //一上来的时候默认就要判断是否全选
+                let isAllCheck = this.cart.every(shop => {
+                    return shop.check === true
+                })
+                this.allCheck.prop('checked', isAllCheck)
+
             }
 
             calcMoney () {
@@ -55,8 +64,41 @@ require(['./config'], function () {
                     localStorage.setItem('cart', JSON.stringify(_this.cart))
                     //调用一次计算总价的方法
                     _this.calcMoney()
+                    //判断是否全选
+                    //判断_this.cart的每一条数据是否都为选中状态
+                    let isAllCheck = _this.cart.every(shop => {
+                        return shop.check === true
+                    })
+                    _this.allCheck.prop('checked', isAllCheck)
                 })
             }
+
+            allCheckChange () {
+                let _this = this
+                this.allCheck.on('change', function () {
+                    //得到全选按钮的状态 isCheck 的状态为true或者false
+                    let isCheck = $(this).prop('checked')
+                    //数据和所有单选都应该按照这个isCheck来修改
+                    _this.cart = _this.cart.map(shop => {
+                        shop.check = isCheck
+                        return shop
+                    })
+                    //把每一个都单选都要切换checked，但jQuery内部已经做好循环过程了，所以只要写下面一句
+                    $('.check-box').prop('checked', isCheck)
+                    //重新计算总价
+                    _this.calcMoney()
+                })
+            }
+
+            //给加减绑事件 事件委托 点了按钮后 1.找父级的tr 找id 将num++或者-- 先改数据
+            plusBtn () {
+                
+            }
+
+
+
+            //删除 也是先找id 然后将从数据删了 然后再将dom移除 
+            //做完加减删除要调用一下header.calcTotalNum()
         }
         new Cart()
     })
